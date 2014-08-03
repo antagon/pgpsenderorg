808c8ebb1c41e75a2ba4785135d7bc82900ca5ff
antagon <antagon@codeward.org>
2014-08-03 15:26:15 +0200

    Unique constraint for user.name.
    Monda-regular removed from the repository.
    
    Style tweaks.

--- db/user.sql
+++ db/user.sql
@@ -1,6 +1,6 @@
 create if not exists table user (
 	id bigint unsigned primary key auto_increment,
-	name varchar (32) not null,
+	name varchar (32) unique not null,
 	password varchar (60) not null,
 	primary_email_id bigint unsigned not null,
 	created timestamp not null
