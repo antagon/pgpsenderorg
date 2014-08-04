b42adec992e04cede4394dc3c575349e4f69ff4d
antagon <antagon@codeward.org>
2014-08-04 20:36:26 +0200

    Template for contact and privacy page.

---
+++ www/contact.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>Contact - PGPSender</title>
+<?php
+	require_once ("include/head.php");
+?>
+	</head>
+	<body>
+		<div id="content">
+			<div id="lang_switcher">
+<?php
+	require_once ("include/top_menu.php");
+?>
+			</div>
+			
+			<h1> <a href="/">PGPSender</a> </h1>
+			<div id="quote">
+<?php
+	require_once ("include/quote.php");
+?>
+			</div>
+
+			<h2>Contact</h2>
+
+			<p>TODO</p>
+
+			</div>
+
+		</div>
+		
+		<div id="footer">
+<?php
+	require_once ("include/footer.php");
+?>
+		</div>
+	</body>
+</html>
+
