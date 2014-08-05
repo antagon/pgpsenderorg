f649e5e056d382e0783e2de55177c23eabfc8934
antagon <antagon@codeward.org>
2014-08-05 18:41:29 +0200

    Template for the user page.

---
+++ www/user.php
@@ -0,0 +1,116 @@
+<?php
+
+session_start ();
+
+if ( ! isset ($_SESSION["user"]) )
+	header ("Location: /login.php");
+	
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>PGPSender</title>
+<?php
+	require_once ("include/head.php");
+?>
+	</head>
+	<body>
+		<div id="content">
+			<div id="lang_switcher">
+<?php
+	require_once ("include/top_menu.php");
+?>
+			</div>
+			
+			<h1> <a href="/">PGPSender</a> </h1>
+			<div id="quote">
+<?php
+	require_once ("include/quote.php");
+?>
+			</div>
+
+			<fieldset>
+				<legend>Emails</legend>
+
+				<div>
+					<button>Add email</button>
+				</div>
+
+				<table>
+					<tbody>
+						<tr> <td>foo@example.org</td> <td class="text-right"> <button disabled="disabled">Primary</button> </td> </tr>
+						<tr> <td>blablah@example.org</td> <td class="text-right"> <button>Set primary</button> </td> </tr>
+					</tbody>
+				</table>
+			</fieldset>
+
+			<fieldset>
+				<legend>Aliases</legend>
+
+				<div>
+					<button>Add alias</button>
+				</div>
+
+				<table>
+					<tbody>
+						<tr>
+							<td>b88d40f311aa0591170a82e9147d3d9c@pgpsender.org</td>
+							<td class="text-right">
+								<select>
+									<option value="null">unassigned</option>
+								</select>
+							</td>
+						</tr>
+						<tr>
+							<td>2be852db88abf04941728fa41cf197ff@pgpsender.org</td>
+							<td class="text-right">
+								<select>
+									<option value="null">unassigned</option>
+								</select>
+							</td>
+						</tr>
+						<tr>
+							<td>3fe7c4bd69d4b53cffe0a9dbe1db6027@pgpsender.org</td>
+							<td class="text-right">
+								<select>
+									<option value="null">unassigned</option>
+								</select>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</fieldset>
+
+			<fieldset>
+				<legend>Change password</legend>
+
+				<input type="password" name="password" placeholder="Password">
+				<input type="password" name="password_confirm" placeholder="Confirm password">
+
+				<button name="submit">Submit</button>
+			</fieldset>
+
+			<fieldset>
+				<legend>Delete account</legend>
+
+				<p class="alert-error">
+					Upon deleting your account all data associated with the account will be deleted from our database!<br>
+					As this operation cannot be reversed, please confirm your decision by providing your password.
+				</p>
+
+				<input type="password" name="password" placeholder="Password">
+
+				<button name="submit">Confirm</button>
+			</fieldset>
+
+		</div>
+		
+		<div id="footer">
+<?php
+	require_once ("include/footer.php");
+?>
+		</div>
+	</body>
+</html>
+
