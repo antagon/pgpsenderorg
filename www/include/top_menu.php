629d6fd39bc923fc55f425f9c2e5b0c72c139c1a
antagon <antagon@codeward.org>
2014-08-05 17:16:13 +0200

    Enable sessions in the pages.
    If a user attempts to access login or register page already
    authenticated, redirect him to index page.
    
    Display user menu.

--- www/include/top_menu.php
+++ www/include/top_menu.php
@@ -1,1 +1,6 @@
-<a href="register.php">Register</a> <span class="delim">|</span> <a href="login.php">Login</a>
+<?php if ( isset ($_SESSION["user"]) ){ ?>
+	<a href="sender.php">Sender</a> <span class="delim">|</span> <a href="user.php">@<?php echo $_SESSION["user"]["name"]; ?></a> <span class="delim">|</span> <a href="logout.php">Logout</a>
+<?php } else { ?>
+	<a href="sender.php">Sender</a> <span class="delim">|</span> <a href="register.php">Register</a> <span class="delim">|</span> <a href="login.php">Login</a>
+<?php } ?>
+
