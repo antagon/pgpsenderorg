7430dec6b4a5d400de4f4ac38f4d65bdddbe8b32
antagon <antagon@codeward.org>
2014-08-05 17:23:49 +0200

    Content of description metatag updated.

--- www/include/head.php
+++ www/include/head.php
@@ -1,8 +1,8 @@
 <link rel="icon" type="image/png" href="img/favicon.ico" />
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-<meta name="description" content="PGPSender.org is disposable mail relay with support for OpenPGP encryption.">
+<meta name="description" content="Libre email cloaker with Pretty Good Privacy.">
 <link rel="image_src" href="img/pgpsender-logo.png">
-<meta name="keywords" content="pgp sender email sendmail online mail gpg relay anonymous openpgp openpgpjs">
+<meta name="keywords" content="pgp sender email sendmail online mail gpg relay anonymous openpgp openpgpjs cloaker cloak">
 <link rel="stylesheet" type="text/css" href="css/style.css?aa=1">
 <script type="text/javascript" src="js/jquery.js"></script>
 <script type="text/javascript" src="js/openpgp.min.js"></script>
