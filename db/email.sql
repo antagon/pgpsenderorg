3c783c88e638603370570f1fdb643801a4bb1253
antagon <antagon@codeward.org>
2014-08-07 21:55:19 +0200

    Email address MUST be unique.
    Use current timestamp if value for the column 'created'
    is not provided.

--- db/email.sql
+++ db/email.sql
@@ -1,7 +1,7 @@
 create table if not exists email (
 	id integer primary key autoincrement,
-	name varchar (254) not null,
+	name varchar (254) unique not null,
 	user_id integer not null,
-	created timestamp not null
+	created timestamp default CURRENT_TIMESTAMP
 );
 
