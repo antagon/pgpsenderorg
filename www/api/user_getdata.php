02822fa1492bd0d45b3ee3157249e7d2e284d92a
antagon <antagon@codeward.org>
2014-08-09 23:24:30 +0200

    Fetch user's aliases and build a data structure.
    The structure newly contains a summary information, these are:
    email count and alias count (future versions may introduce
    more fields).

--- www/api/user_getdata.php
+++ www/api/user_getdata.php
@@ -4,6 +4,7 @@ require_once ("../include/ApiResponse.class.php");
 require_once ("../include/Config.class.php");
 require_once ("../include/User.class.php");
 require_once ("../include/Email.class.php");
+require_once ("../include/Alias.class.php");
 
 if ( ! isset ($_POST["api_key"]) || empty ($_POST["api_key"]) )
 	ApiResponse::json_exit (ApiResponse::E_EMPTY, "API key is empty");
@@ -17,23 +18,34 @@ if ( $user_data === false )
 	ApiResponse::json_exit (ApiResponse::E_UNAUTHORIZED, "Unauthorized");
 
 $email_db = new Email (Config::SQLITE3_DB_FILE);
+$alias_db = new Alias (Config::SQLITE3_DB_FILE);
 
 $emails = $email_db->get_for_uid ($user_data["id"]);
+$aliases = $alias_db->get_for_uid ($user_data["id"]);
 
 if ( $emails === false )
 	ApiResponse::json_exit (ApiResponse::E_INTERNAL);
 
-$data = array ();
+if ( $aliases === false )
+	ApiResponse::json_exit (ApiResponse::E_INTERNAL);
+
+$json_array = array ("summary" => array ("address_count" => count ($emails),
+									"alias_count" => count ($aliases)),
+									"data" => array ());
 
 foreach ( $emails as $email ){
-	$data[] = array ("address" => $email["name"], "alias" => array ());
+
+	// Initialize temporary alias array
+	$email["alias"] = array ();
+
+	foreach ( $aliases as $alias ){
+		if ( $emails["id"] == $alias["email_id"] )
+			$email["alias"][] = $alias["name"];
+	}
+
+	$json_array["data"][] = array ("name" => $email["name"], "alias" => $email["alias"]);
 }
 
-/*$data = array (
-	array ("address" => "nobody@example.com", "alias" => array ("b88d40f311aa0591170a82e9147d3d9c@pgpsender.org", "2be852db88abf04941728fa41cf197ff@pgpsender.org")),
-	array ("address" => "johndoe@example.com", "alias" => array ("3fe7c4bd69d4b53cffe0a9dbe1db6027@pgpsender.org"))
-);*/
-
-ApiResponse::json_exit_data (ApiResponse::E_OK, $data);
+ApiResponse::json_exit_data (ApiResponse::E_OK, $json_array);
 ?>
 
