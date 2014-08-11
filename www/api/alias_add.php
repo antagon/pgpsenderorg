686c7dbb05e502bcfd76895817c64e095bb265ae
antagon <antagon@codeward.org>
2014-08-11 21:39:16 +0200

    Expect and process hostname.
    Alias name should be a concatenation of user's input and one of
    the preconfigured hostnames.

--- www/api/alias_add.php
+++ www/api/alias_add.php
@@ -8,11 +8,22 @@ require_once ("../include/Alias.class.php");
 if ( ! isset ($_POST["api_key"]) || empty ($_POST["api_key"]) )
 	ApiResponse::json_exit (ApiResponse::E_EMPTY, "API key is empty");
 
-if ( ! isset ($_POST["address"]) || empty ($_POST["address"]) )
+if ( ! isset ($_POST["alias"]) || empty ($_POST["alias"]) )
 	ApiResponse::json_exit (ApiResponse::E_EMPTY, "Address is empty");
 
-if ( filter_var($_POST["address"], FILTER_VALIDATE_EMAIL) === false )
-	ApiResponse::json_exit (ApiResponse::E_FORMAT, "Invalid address format");
+if ( ! isset ($_POST["hostname"]) || empty ($_POST["hostname"]) )
+	ApiResponse::json_exit (ApiResponse::E_EMPTY, "Hostname is empty");
+
+// Check if user provided the alias in email address format, probably out of confusion.
+// We should not punish him for that, but rather fix it for him.
+$addr_tokens = explode ("@", $_POST["alias"]);
+$_POST["alias"] = $addr_tokens[0];
+
+// Check if hostname exists in configuration
+if ( in_array ($_POST["hostname"], Config::$hostname) === false )
+	ApiResponse::json_exit (ApiResponse::E_NEXISTS, "Hostname does not exists");
+
+$_POST["alias"] .= "@".$_POST["hostname"];
 
 $user_db = new User (Config::SQLITE3_DB_FILE);
 
@@ -24,10 +35,10 @@ if ( $user_data === false )
 
 $alias_db = new Alias (Config::SQLITE3_DB_FILE);
 
-if ( $alias_db->exists ($_POST["address"]) )
+if ( $alias_db->exists ($_POST["alias"]) )
 	ApiResponse::json_exit (ApiResponse::E_EXISTS, "Alias already exists");
 
-if ( $alias_db->create ($_POST["address"], $user_data["id"]) === false )
+if ( $alias_db->create ($_POST["alias"], $user_data["id"]) === false )
 	ApiResponse::json_exit (ApiResponse::E_INTERNAL);
 
 ApiResponse::json_exit (ApiResponse::E_OK);
