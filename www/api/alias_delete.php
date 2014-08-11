0ae93a49252ca1a398408b63997bec5e0e56ccd4
antagon <antagon@codeward.org>
2014-08-11 21:36:06 +0200

    FIX: check alias ownership.
    Use Alias::owns () instead of Alias::exists ().
    
    FIX: Alias::delete () requires a second parameter.

--- www/api/alias_delete.php
+++ www/api/alias_delete.php
@@ -8,7 +8,7 @@ require_once ("../include/Alias.class.php");
 if ( ! isset ($_POST["api_key"]) || empty ($_POST["api_key"]) )
 	ApiResponse::json_exit (ApiResponse::E_EMPTY, "API key is empty");
 
-if ( ! isset ($_POST["address"]) || empty ($_POST["address"]) )
+if ( ! isset ($_POST["alias"]) || empty ($_POST["alias"]) )
 	ApiResponse::json_exit (ApiResponse::E_EMPTY, "Address is empty");
 
 $user_db = new User (Config::SQLITE3_DB_FILE);
@@ -21,10 +21,10 @@ if ( $user_data === false )
 
 $alias_db = new Alias (Config::SQLITE3_DB_FILE);
 
-if ( $alias_db->exists ($_POST["address"]) === false )
+if ( $alias_db->owns ($_POST["alias"], $user_data["id"]) === false )
 	ApiResponse::json_exit (ApiResponse::E_NEXISTS, "Alias does not exists");
 
-if ( $alias_db->delete ($_POST["address"]) === false )
+if ( $alias_db->delete ($_POST["alias"], $user_data["id"]) === false )
 	ApiResponse::json_exit (ApiResponse::E_INTERNAL);
 
 ApiResponse::json_exit (ApiResponse::E_OK);
