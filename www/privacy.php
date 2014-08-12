8a0794d996651f5b1d95c474fb44e27259289c6c
antagon <antagon@codeward.org>
2014-08-12 22:49:44 +0200

    Tenets of the privacy policy.

--- www/privacy.php
+++ www/privacy.php
@@ -27,7 +27,18 @@
 
 			<h2>Privacy</h2>
 
-			<p>TODO</p>
+			<p>This document lays down a key tenets of our privacy policy in respect to user and user's data.</p>
+
+			<ol>
+				<li>We <strong>shall not</strong> expose user's email addresses to anyone.</li>
+				<li>We <strong>shall not</strong> expose relationship between user defined aliases and user's email addresses.</li>
+				<li>We <strong>shall not</strong> prowl through content of user's emails.</li>
+				<li>We <strong>shall not</strong> preserve undeliverable emails.</li>
+				<li>We <strong>shall not</strong> preserve user's email addresses against his will.</li>
+				<li>We <strong>shall not</strong> monitor user's activity and/or behavior.</li>
+				<li>We <strong>shall not</strong> send unsolicited emails to user.</li>
+				<li>We <strong>shall</strong> reveal publicly all attempts to break our vow to these tenets.</li>
+			</ol>
 
 		</div>
 		
