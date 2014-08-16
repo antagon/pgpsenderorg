0c7636ac8a1693e6744f12b5ab8d30fd3e393303
antagon <antagon@codeward.org>
2014-08-16 21:26:21 +0200

    Font Awesome styles & fonts.

--- www/include/head.php
+++ www/include/head.php
@@ -3,7 +3,10 @@
 <meta name="description" content="Libre email cloaker with Pretty Good Privacy.">
 <link rel="image_src" href="img/pgpsender-logo.png">
 <meta name="keywords" content="pgp sender email sendmail online mail gpg relay anonymous openpgp openpgpjs cloaker cloak">
-<link rel="stylesheet" type="text/css" href="css/style.css?aa=1">
+
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/font-awesome/font-awesome.min.css">
+
 <script type="text/javascript" src="js/jquery.js"></script>
 <script type="text/javascript" src="js/openpgp.min.js"></script>
 <script type="text/javascript" src="js/pgpsender_api.js"></script>
